430fe60650892d64c9eaab91075d7f7fdeed0df9
Radu <radu.bordei89@gmail.com>
2015-10-01 10:42:01 +0300

    adding config files and migrations.

---
+++ migrations/2015_09_02_093148_create_attachments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttachmentsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        if (Schema::hasTable('attachments')) {
+            Schema::create('attachments', function(Blueprint $table) {
+                $table->integer('imageable_id');
+                $table->string('imageable_type');
+                $table->string('title');
+                $table->string('path');
+                $table->string('full_path');
+                $table->string('extension');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        if (Schema::hasTable('attachments')) {
+            Schema::drop('attachments');
+        }
+    }
+}
