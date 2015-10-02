649269be74eaeefed575c72d29c98188ef594057
Radu <radu.bordei89@gmail.com>
2015-10-02 09:59:38 +0300

    improve image manager.

--- src/Controllers/ImageController.php
+++ src/Controllers/ImageController.php
@@ -3,10 +3,23 @@
 namespace Flysap\ImageManager\Controllers;
 
 use App\Http\Controllers\Controller;
+use Eloquent\ImageAble\Attachment;
+use Flysap\TableManager;
 
 class ImageController extends Controller {
 
+    /**
+     * Lists all images .
+     *
+     * @return \Illuminate\View\View
+     */
     public function lists() {
 
+        $table = TableManager\table((new Attachment), 'eloquent', ['class' => 'table table-hover']);
+
+        return view('themes::pages.table', [
+            'title' => _('Images'),
+            'table' => $table
+        ]);
     }
 }
