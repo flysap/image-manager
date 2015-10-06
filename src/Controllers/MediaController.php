12aaec2c2885612aace073277b1cb7e0eb301b3b
Radu <radu.bordei89@gmail.com>
2015-10-06 14:15:19 +0300

    improve mediaController.

--- src/Controllers/MediaController.php
+++ src/Controllers/MediaController.php
@@ -3,6 +3,7 @@
 namespace Flysap\Media\Controllers;
 
 use App\Http\Controllers\Controller;
+use Cartalyst\Tags\TaggableInterface;
 use Eloquent\Translatable\Translatable;
 use Flysap\Media\Media;
 use Parfumix\TableManager;
@@ -49,7 +50,9 @@ DOC;
     /**
      * Adding create logic business .
      *
+     * @param Request $request
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
+     * @throws FormBuilder\ElementException
      */
     public function create(Request $request) {
         if(! $_POST) {
@@ -83,6 +86,15 @@ DOC;
                 }
             }
 
+            if( $this->repository instanceof TaggableInterface ) {
+                $addTag = FormBuilder\element_text(_('New tags'), [
+                    'name'  => 'tags[]',
+                    'group' => 'tags'
+                ]);
+
+                $elements[] = $addTag;
+            }
+
             $form->addElements($elements, true);
 
             return view('scaffold::scaffold.edit', compact('form'));
@@ -94,6 +106,18 @@ DOC;
 
         $mediaRow = $this->repository
             ->create($_POST);
+
+        array_walk($images, function($image) use($mediaRow) {
+            $mediaRow
+                ->fill([
+                    'path' => $image->relative_path,
+                    'full_path' => $image->dirname . DIRECTORY_SEPARATOR . $image->basename,
+                ])->save();
+        });
+
+        if( $mediaRow instanceof TaggableInterface )
+            if( isset($params['tags']) )
+                $mediaRow->setTags($params['tags']);
 
         return redirect(
             route('media_edit', ['id' => $mediaRow->id])
@@ -119,9 +143,80 @@ DOC;
      * Edit by id .
      *
      * @param $id
+     * @return \Illuminate\View\View
      */
     public function edit($id) {
+        $mediaRow = $this->repository
+            ->find($id);
 
+        if(! $_POST) {
+            $form = FormBuilder\create_form([
+                'action' => route('media_edit', ['id' => $id]),
+                'enctype' => FormBuilder\Form::ENCTYPE_MULTIPART,
+                'method' => FormBuilder\Form::METHOD_POST
+            ]);
+
+            $elements[] = FormBuilder\element_file('file', [
+                'name'  => 'file',
+                'group' => 'default'
+            ]);
+
+            $elements[] = FormBuilder\element_checkbox('active', [
+                'name'  => 'active',
+                'group' => 'default',
+                'value' => $mediaRow->active
+            ]);
+
+            if( $mediaRow instanceof Translatable ) {
+                $locales = Locale\get_locales();
+
+                foreach($locales as $locale => $attributes) {
+                    $translation = $mediaRow->translate($locale);
+
+                    foreach($this->repository->translatedAttributes() as $attribute => $type) {
+                        $elements[]  = FormBuilder\get_element($type, [
+                            'group' => 'translations',
+                            'label' => ucfirst($attribute) . ' ' . $locale,
+                            'name'  => $locale . '['.$attribute.']',
+                            'value' => isset($translation[$attribute]) ? $translation[$attribute] : '',
+                        ]);
+                    }
+                }
+            }
+
+            if( $mediaRow instanceof TaggableInterface ) {
+                $tags = $mediaRow->tags;
+
+                foreach($tags as $tag)
+                    $elements[]  = FormBuilder\get_element('text', [
+                        'before' => '<a href="#" onclick="$(this).closest(\'div\').remove(); return false;">'._('Remove').'</a>',
+                        'name'   => 'tags[]',
+                        'group'  => 'tags',
+                        'value'  => $tag->getAttribute('name'),
+                    ]);
+
+                $addTag = FormBuilder\element_text(_('New tags'), [
+                    'name'  => 'tags[]',
+                    'group' => 'tags'
+                ]);
+
+                $elements[] = $addTag;
+            }
+
+            $form->addElements($elements, true);
+
+            return view('scaffold::scaffold.edit', compact('form'));
+        }
+
+        $mediaRow
+            ->fill($_POST)
+            ->save();
+
+        if( $mediaRow instanceof TaggableInterface )
+            if( isset($_POST['tags']) )
+                $mediaRow->setTags($_POST['tags']);
+
+        return back();
     }
 
     /**
