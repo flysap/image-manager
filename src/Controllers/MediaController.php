11deec673795dc91a331e46c9009bd46361964c1
Radu <radu.bordei89@gmail.com>
2015-10-06 12:24:38 +0300

    improve mediaController.

--- src/Controllers/MediaController.php
+++ src/Controllers/MediaController.php
@@ -3,9 +3,12 @@
 namespace Flysap\Media\Controllers;
 
 use App\Http\Controllers\Controller;
+use Eloquent\Translatable\Translatable;
 use Flysap\Media\Media;
 use Parfumix\TableManager;
 use Parfumix\FormBuilder;
+use Localization as Locale;
+use Illuminate\Http\Request;
 
 class MediaController extends Controller {
 
@@ -48,22 +51,46 @@ DOC;
      *
      * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
      */
-    public function create() {
+    public function create(Request $request) {
         if(! $_POST) {
             $form = FormBuilder\create_form([
                 'action' => route('media_create'),
+                'enctype' => FormBuilder\Form::ENCTYPE_MULTIPART,
                 'method' => FormBuilder\Form::METHOD_POST
             ]);
 
-            $elements[] = FormBuilder\element_text('slug', [
-                'name'  => 'slug',
+            $elements[] = FormBuilder\element_file('file', [
+                'name'  => 'file',
                 'group' => 'default'
             ]);
 
-            $form->addElements($elements);
+            $elements[] = FormBuilder\element_checkbox('active', [
+                'name'  => 'active',
+                'group' => 'default'
+            ]);
+
+            if( $this->repository instanceof Translatable ) {
+                $locales = Locale\get_locales();
+
+                foreach($locales as $locale => $attributes) {
+                    foreach($this->repository->translatedAttributes() as $attribute => $type) {
+                        $elements[]  = FormBuilder\get_element($type, [
+                            'group' => 'translations',
+                            'label' => ucfirst($attribute) . ' ' . $locale,
+                            'name'  => $locale . '['.$attribute.']',
+                        ]);
+                    }
+                }
+            }
+
+            $form->addElements($elements, true);
 
             return view('scaffold::scaffold.edit', compact('form'));
         }
+
+        $images = $this->upload(
+            $request->file('file')
+        );
 
         $mediaRow = $this->repository
             ->create($_POST);
@@ -95,5 +122,40 @@ DOC;
      */
     public function edit($id) {
 
+    }
+
+    /**
+     * Upload images .
+     *
+     * @param array $images
+     * @param null $path
+     * @param array $filters
+     * @return array
+     */
+    protected function upload($images = array(), $path = null, $filters = array()) {
+        if(! is_array($images))
+            $images = [$images];
+
+        $processor = $this->getProcessor();
+
+        if( is_null($path) )
+            $path = config('media-manager.store_path', '');
+
+        $path = storage_path($path);
+
+        $filters = array_merge($filters, config('media-manager.filters'));
+
+        return $processor->upload(
+            $images, $path, $filters
+        );
+    }
+
+    /**
+     * Get image processor .
+     *
+     * @return \Illuminate\Foundation\Application|mixed
+     */
+    protected function getProcessor() {
+        return app('image-processor');
     }
 }
