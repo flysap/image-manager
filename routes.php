80818a57655a0a3f4e2943d7f7e9533e4efc7a97
Radu <radu.bordei89@gmail.com>
2015-10-07 12:05:20 +0300

    adding crop route.

--- routes.php
+++ routes.php
@@ -5,5 +5,6 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Flysap\Media\Controllers', 'm
     Route::get('media', ['as' => 'media', 'uses' => 'MediaController@lists']);
     Route::any('media/create', ['as' => 'media_create', 'uses' => 'MediaController@create']);
     Route::any('media/edit/{id}', ['as' => 'media_edit', 'uses' => 'MediaController@edit']);
+    Route::any('media/crop/{id}', ['as' => 'media_crop', 'uses' => 'MediaController@crop']);
     Route::get('media/delete/{id}', ['as' => 'media_delete', 'uses' => 'MediaController@delete']);
 });
